34265917ba46a82b439486e32789bfc4c877956a
Croath <croathliu@gmail.com>
2013-10-03 16:51:19 +0800

    basic menu added

--- mrdict/mrdict/AppDelegate.m
+++ mrdict/mrdict/AppDelegate.m
@@ -8,11 +8,29 @@
 
 #import "AppDelegate.h"
 
+@interface AppDelegate(){
+    NSTimer *_timer;
+}
+
+@end
+
 @implementation AppDelegate
 
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification
 {
     // Insert code here to initialize your application
+    [self activateStatusMenu];
+}
+
+- (void)activateStatusMenu
+{
+    NSStatusBar *bar = [NSStatusBar systemStatusBar];
+    NSStatusItem *theItem = [bar statusItemWithLength:NSVariableStatusItemLength];
+    
+    [theItem setTitle:@"(´･_･`)"];
+    [theItem setHighlightMode:YES];
+    [theItem setMenu:_menu];
+    _statusItem = theItem;
 }
 
 @end
